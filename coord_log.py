bd1d400977987c4010e47ac1d4c23253bfaebe09
Arnz3 <cuyversarno@gmail.com>
2022-02-09 19:40:51 +0100

    coord-check

---
+++ coord_log.py
@@ -0,0 +1,21 @@
+from curses import baudrate
+import serial
+from ublox_gps import UbloxGps
+
+port = serial.Serial('/dev/ttyACM0', baudrate=38400, timeout=1)
+gps = UbloxGps(port)
+
+nrml_lat = 2
+nrml_lon = 51
+
+while True:
+    geo = gps.geo_coords()
+    lat = geo.lat
+    lon = geo.lon
+
+    if int(lat.split('.')[0]) == nrml_lat and int(lon.split('.')[1]) == nrml_lon:
+        print(lat + " , " + lon + " > gecallibreerd")
+
+    else:
+        print(lat + " , " + lon + " > niet gecallibreerd")
+        
