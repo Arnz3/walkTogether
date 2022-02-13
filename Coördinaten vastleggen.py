837ae80f6a6b6178717f6f215a2be2cfc109127a
Arnz3 <cuyversarno@gmail.com>
2022-02-13 21:26:33 +0100

    logger script

--- Coördinaten vastleggen.py
+++ Coördinaten vastleggen.py
@@ -1,21 +1,23 @@
 import csv
 import time
-import os
 from ublox_gps import UbloxGps
 import serial
+from datetime import datetime
+import RPi.GPIO as GPIO
 port = serial.Serial('/dev/serial0', baudrate=38400, timeout=1)
 gps = UbloxGps(port)
-import RPi.GPIO as GPIO
-import sys
-from serial import SerialException
 
-f = open('/home/pi/Downloads/gps/vastgelegde_punten.csv', 'w')
+f = open('docs/', 'w')
 writer =  csv.writer(f)
-writer.writerow(['lat', 'lon'])
-
+writer.writerow(['lat', 'lon', 'time'])
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN)
 
 lon_gps = 0.0
 lat_gps = 0.0
+
+run = True
+waitTime = 3
 
 def getCurrentLocation():
     print('loc')
@@ -28,17 +30,25 @@ def getCurrentLocation():
     print(geo.lat)
     print(geo.lon)
 
+
 def leg_punt_vast():
     getCurrentLocation()
     print(lat_gps + lon_gps)
+    time = datetime.now().strftime("%H-%M-%S")
     writer.writerow([lat_gps, lon_gps])
-    
+
+
 def stop():
     f.close()
     exit()
 
-if lon_gps > 51 and lat_gps > 2:
-    for i in range (0,10):
-        leg_punt_vast()
-        time.sleep(60)
-    stop()
+
+def checkHalt():
+    if(GPIO.input(21)):
+        stop()
+
+
+while run:
+    leg_punt_vast()
+    checkHalt()
+    time.sleep(waitTime)
