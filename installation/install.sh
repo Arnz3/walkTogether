ecd0287f5d65d533a6e82d1da9073e9dae1daaa9
Arnz3 <cuyversarno@gmail.com>
2022-06-01 20:53:44 +0200

    install file

---
+++ installation/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# turn on bluetooth
+sudo hciconfig hci0 up
+sudo btmgmt le on
+
+#move service file to system dir
+sudo mv -f wandeling.service /lib/systemd/system/
+
+#permissions
+sudo chmod 644 /lib/systemd/system/wandeling.service
+
+#reload deamon
+sudo systemctl deamon-reload
+sudo systemctl enable wandeling.service
+
+#install pip packages
+sudo pip3 install -r requirements.txt 
