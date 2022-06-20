699d7f9d14d5352e79f7a03c4391a3e964458961
Arnz3 <cuyversarno@gmail.com>
2022-06-20 22:26:37 +0200

    log messages

--- installation/install.sh
+++ installation/install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 # turn on bluetooth
+echo "[LOG]: enabling bluetooth low energy..."
 sudo hciconfig hci0 up
 sudo btmgmt le on
 
 #move service file to system dir
+echo "[LOG]: creating service file..."
 sudo mv -f wandeling.service /lib/systemd/system/
 
 #permissions
 sudo chmod 644 /lib/systemd/system/wandeling.service
 
 #reload deamon
+echo "[LOG]: setting up daemon..."
 sudo systemctl daemon-reload
 sudo systemctl enable wandeling.service
 
 #install pip packages
+echo "[LOG]: installing pip packages..."
 sudo pip3 install -r requirements.txt 
+
+echo "DONE!!!"
